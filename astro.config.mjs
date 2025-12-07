731fbe9a0fe761c43331fca16e6dedba9ce83c49
jomiferse <jomiferse@gmail.com>
2025-12-07 01:40:31 +0100

    chore: update Astro config and migrate project to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
@@ -9,4 +11,5 @@ export default defineConfig({
   site: "https://www.jomiferse.com",
   integrations: [react()],
   output: "static",
+  adapter: cloudflare(),
 });
