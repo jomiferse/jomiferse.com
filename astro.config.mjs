9fe05b45889354ebcac93fa4ce4429616fb16168
jomiferse <jomiferse@gmail.com>
2025-12-13 01:19:06 +0100

    chore: add astro-icon integration and update component imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
 	site: "https://www.jomiferse.com",
-	integrations: [react()],
+	integrations: [react(), icon()],
 	output: "static"
 });
