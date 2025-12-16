d9f1c247c72c488c221efe3b2e04fc929627a9b7
jomiferse <jomiferse@gmail.com>
2025-12-17 00:05:51 +0100

    chore(seo): add automatic sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,25 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
 	site: "https://www.jomiferse.com",
-	integrations: [icon()],
+	integrations: [
+		icon(),
+		sitemap({
+			filter: (page) => {
+				return (
+					!page.startsWith("/api/") &&
+					!page.startsWith("/_astro/") &&
+					!page.startsWith("/drafts/")
+				);
+			},
+		}),
+	],
 	output: "static",
 	adapter: vercel(),
 	darkMode: "class",
